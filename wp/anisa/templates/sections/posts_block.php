fb3e2cb69eee1a2eb74f62afad4787fe9e7e48a2
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-08-01 13:20:43 +0200

    posts

---
+++ wp/anisa/templates/sections/posts_block.php
@@ -0,0 +1,124 @@
+<?php
+
+$title = get_sub_field('title');
+$news = get_sub_field('news');
+
+?>
+
+<section class="slider-block">
+    <div class="content-width">
+        <?php if($title):?>
+            <h2><?= $title;?></h2>
+        <?php endif;?>
+        <div class="slider-wrap">
+            <div class="swiper home-slider">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <figure>
+                            <a href="#">
+                                <img src="img/img-7-1.jpg" alt="">
+                            </a>
+                        </figure>
+                        <div class="text">
+                            <h6 class="label">anisa foundation</h6>
+                            <h3><a href="#">How to Prop Up Products With Blocks to Create Eye-Catching Product Photos</a></h3>
+                            <p>Add dimension and personality to your images with blocks, boxes, cubes, and other photography props.</p>
+                            <ul>
+                                <li><img src="img/icon-2.svg" alt="">10 Min read</li>
+                                <li><p class="date">October 20, 2021</p></li>
+                                <li><p class="name">by Laura Bauer</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <figure>
+                            <a href="#">
+                                <img src="img/img-7-2.jpg" alt="">
+                            </a>
+                        </figure>
+                        <div class="text">
+                            <h6 class="label">anisa foundation</h6>
+                            <h3><a href="#">How to Prop Up Products With Blocks to Create Eye-Catching Product Photos</a></h3>
+                            <p>Add dimension and personality to your images with blocks, boxes, cubes, and other photography props.</p>
+                            <ul>
+                                <li><img src="img/icon-2.svg" alt="">10 Min read</li>
+                                <li><p class="date">October 20, 2021</p></li>
+                                <li><p class="name">by Laura Bauer</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <figure>
+                            <a href="#">
+                                <img src="img/img-7-3.jpeg" alt="">
+                            </a>
+                        </figure>
+                        <div class="text">
+                            <h6 class="label">anisa foundation</h6>
+                            <h3><a href="#">How to Prop Up Products With Blocks to Create Eye-Catching Product Photos</a></h3>
+                            <p>Add dimension and personality to your images with blocks, boxes, cubes, and other photography props.</p>
+                            <ul>
+                                <li><img src="img/icon-2.svg" alt="">10 Min read</li>
+                                <li><p class="date">October 20, 2021</p></li>
+                                <li><p class="name">by Laura Bauer</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <figure>
+                            <a href="#">
+                                <img src="img/img-7-1.jpg" alt="">
+                            </a>
+                        </figure>
+                        <div class="text">
+                            <h6 class="label">anisa foundation</h6>
+                            <h3><a href="#">How to Prop Up Products With Blocks to Create Eye-Catching Product Photos</a></h3>
+                            <p>Add dimension and personality to your images with blocks, boxes, cubes, and other photography props.</p>
+                            <ul>
+                                <li><img src="img/icon-2.svg" alt="">10 Min read</li>
+                                <li><p class="date">October 20, 2021</p></li>
+                                <li><p class="name">by Laura Bauer</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <figure>
+                            <a href="#">
+                                <img src="img/img-7-2.jpg" alt="">
+                            </a>
+                        </figure>
+                        <div class="text">
+                            <h6 class="label">anisa foundation</h6>
+                            <h3><a href="#">How to Prop Up Products With Blocks to Create Eye-Catching Product Photos</a></h3>
+                            <p>Add dimension and personality to your images with blocks, boxes, cubes, and other photography props.</p>
+                            <ul>
+                                <li><img src="img/icon-2.svg" alt="">10 Min read</li>
+                                <li><p class="date">October 20, 2021</p></li>
+                                <li><p class="name">by Laura Bauer</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <figure>
+                            <a href="#">
+                                <img src="img/img-7-3.jpeg" alt="">
+                            </a>
+                        </figure>
+                        <div class="text">
+                            <h6 class="label">anisa foundation</h6>
+                            <h3><a href="#">How to Prop Up Products With Blocks to Create Eye-Catching Product Photos</a></h3>
+                            <p>Add dimension and personality to your images with blocks, boxes, cubes, and other photography props.</p>
+                            <ul>
+                                <li><img src="img/icon-2.svg" alt="">10 Min read</li>
+                                <li><p class="date">October 20, 2021</p></li>
+                                <li><p class="name">by Laura Bauer</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-button-next home-next btn"><img src="img/icon-1.svg" alt=""></div>
+                <div class="swiper-button-prev home-prev btn"><img src="img/icon-1.svg" alt=""></div>
+            </div>
+        </div>
+    </div>
+</section>
